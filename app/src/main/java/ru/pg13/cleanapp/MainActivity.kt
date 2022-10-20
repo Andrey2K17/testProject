1a966a71bbb6441ad03dcf9cdffd09ff1ac2fb18
= <nba.grinin.96@gmail.com>
2022-10-20 22:51:17 +0700

    first commit

---
+++ app/src/main/java/ru/pg13/cleanapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.pg13.cleanapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
